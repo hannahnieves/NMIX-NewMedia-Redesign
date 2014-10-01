95c08bde92b8c48f32b745ed2cc2d170d066a30d
JEFF GROSS <jeffgross92@gmail.com>
2014-10-01 17:26:54 -0400

    thumbnails

--- css/bootstrap.js
+++ css/bootstrap.js
@@ -2111,4 +2111,19 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
     })
   })
 
+$( document ).ready(function() {
+    $("[rel='tooltip']").tooltip();    
+ 
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
+});
+
+
+
 }(jQuery);
